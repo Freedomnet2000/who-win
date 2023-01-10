4a18e7c2cb05631e57ed346bfd15cf3cf16f4227
Freedomnet2000 <34886637+Freedomnet2000@users.noreply.github.com>
2023-01-10 15:53:04 +0200

    done-final

--- frequenct-level.php
+++ frequenct-level.php
@@ -13,26 +13,26 @@ function innerCount($arrayOfInt,$search,$i = 0)
     return[$i,$count];
 }
 
-    function freqCount($input,$search,$i = 0, $res=[]) {
-        $arrayToMerge = [];
-        $tmpValArray = [];
-        foreach ($input as $val) {
-            if (!is_array($val)) {
-                $tmpValArray[] = $val;
-            }
-            else{
-                foreach ($val as $innerVal) {
-                    $arrayToMerge[] =  $innerVal;
-                }
+function freqCount($input,$search,$i = 0, $res=[]) {
+    $arrayToMerge = [];
+    $tmpValArray = [];
+    foreach ($input as $val) {
+        if (!is_array($val)) {
+            $tmpValArray[] = $val;
+        }
+        else{
+            foreach ($val as $innerVal) {
+                $arrayToMerge[] =  $innerVal;
             }
         }
-        $res[] = innerCount($tmpValArray,$search,$i);
-        if (count($arrayToMerge)>0){
-            $i++;
-            $res = freqCount($arrayToMerge,$search,$i,$res);
-        }
-        return  $res;
     }
+    $res[] = innerCount($tmpValArray,$search,$i);
+    if (count($arrayToMerge)>0){
+        $i++;
+        $res = freqCount($arrayToMerge,$search,$i,$res);
+    }
+    return  $res;
+}
 
 
 //print_r(freqCount([1,4,4,[1,1,[1,2,1,1]]], 1));
